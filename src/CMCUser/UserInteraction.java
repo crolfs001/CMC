dad1b9a26f587cf2e20d575a4ded32e4cf408466
DMURPHY001 <dmurphy001@vplincsci025.ad.csbsju.edu>
2020-02-25 18:43:10 -0600

    UserInteraction created 'logout' method

--- src/CMCUser/UserInteraction.java
+++ src/CMCUser/UserInteraction.java
@@ -17,7 +17,33 @@ public class UserInteraction {
 	 * @param user the User object for this UserInteraction
 	 */
 	public UserInteraction(User user) {
+		this.setUser(user);
+	}
+
+	/**
+	 * user getter
+	 * @return user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * user setter
+	 * @param user the new user for this UserInteraction object
+	 */
+	public void setUser(User user) {
 		this.user = user;
 	}
+	
+	/**
+	 * logs the current user out of the system
+	 * @return a boolean showing the user is logged out
+	 */
+	public boolean logout() {
+		user.setLoginStatus(false);
+		return user.getLoginStatus();
+	}
+	
 
 }
