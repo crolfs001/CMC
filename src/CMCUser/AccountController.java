8b207989d627839f69be85f8f2fc3a71e3b87e9f
DMURPHY001 <dmurphy001@vplincsci011.ad.csbsju.edu>
2020-02-26 06:49:03 -0600

    create getter and setter methods for AccountController class

--- src/CMCUser/AccountController.java
+++ src/CMCUser/AccountController.java
@@ -18,7 +18,43 @@ public class AccountController {
 	 * Constructs an AccountController object
 	 */
 	public AccountController(DBController database) {
+		this.setDatabase(database);
+	}
+
+
+	/**
+	 * database getter
+	 * @return the database
+	 */
+	public DBController getDatabase() {
+		return database;
+	}
+
+
+	/**
+	 * database setter
+	 * @param database the new database for this AccountController
+	 */
+	public void setDatabase(DBController database) {
 		this.database = database;
+	}
+
+
+	/**
+	 * user getter
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+
+	/**
+	 * user setter
+	 * @param user the new user for this AccountController
+	 */
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 }
