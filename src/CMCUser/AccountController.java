3e0b98404e2165bc94b1b89f8c4443895a5b5d76
DMURPHY001 <dmurphy001@vplincsci025.ad.csbsju.edu>
2020-02-25 19:35:21 -0600

    created constructor with attributes for AccountController class

--- src/CMCUser/AccountController.java
+++ src/CMCUser/AccountController.java
@@ -1,19 +1,24 @@
 /**
- * 
+ * The AccountController class is used to manage interactions between a type of UserInteraction 
+ * object, the database, and User objects.
  */
 package CMCUser;
+import CMCAdmin.DBController;
 
 /**
- * @author dmurphy001
+ * @author Devin Murphy
  *
  */
 public class AccountController {
+	private DBController database;
+	private User user;
+	
 
 	/**
-	 * 
+	 * Constructs an AccountController object
 	 */
-	public AccountController() {
-		// TODO Auto-generated constructor stub
+	public AccountController(DBController database) {
+		this.database = database;
 	}
 
 }
