0ea22bea1bd2b0896813b6bf7f80bce7236d3a16
DMURPHY001 <dmurphy001@vplincsci025.ad.csbsju.edu>
2020-02-25 18:10:56 -0600

    create getters and setters for User's attributes

--- src/CMCUser/User.java
+++ src/CMCUser/User.java
@@ -25,13 +25,97 @@ public class User {
 	 */
 	public User(String firstName, String lastName, String userName, String password, 
 			char type) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.userName = userName;
-		this.password = password;
-		this.type = type;
-		this.status = 'Y'; //default is activated
-		this.loginStatus = false; //default is user is not logged in
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setUserName(userName);
+		this.setPassword(password);
+		this.setType(type);
+		this.setStatus('Y'); //default is activated
+		this.setLoginStatus(false); //default is user is not logged in
 	}
+
+	/**
+	 * firstName getter
+	 * @return firstName
+	 */
+	public String getFirstName() {return firstName;}
+
+	/**
+	 * firstName setter
+	 * @param firstName the new firstName of this User
+	 */
+	public void setFirstName(String firstName) {this.firstName = firstName;}
+
+	/**
+	 * lastName getter
+	 * @return lastName
+	 */
+	public String getLastName() {return lastName;}
+
+	/**
+	 * lastName setter
+	 * @param lastName the new lastName for this User
+	 */
+	public void setLastName(String lastName) {this.lastName = lastName;}
+
+	/**
+	 * userName getter
+	 * @return userName
+	 */
+	public String getUserName() {return userName;}
+
+	/**
+	 * userName setter
+	 * @param userName the new userName for this User
+	 */
+	public void setUserName(String userName) {this.userName = userName;}
+
+	/**
+	 * password getter
+	 * @return password
+	 */
+	public String getPassword() {return password;}
+
+	/**
+	 * password setter
+	 * @param password the new password for this User
+	 */
+	public void setPassword(String password) {this.password = password;}
+
+	/**
+	 * type getter
+	 * @return type
+	 */
+	public char getType() {return type;}
+
+	/**
+	 * type setter
+	 * @param type the new type for this User
+	 */
+	public void setType(char type) {this.type = type;}
+
+	/**
+	 * status getter
+	 * @return status
+	 */
+	public char getStatus() {return status;}
+
+	/**
+	 * status setter
+	 * @param status the new status for this User
+	 */
+	public void setStatus(char status) {this.status = status;}
+
+	/**
+	 * loginStatus getter
+	 * @return loginStatus
+	 */
+	public boolean isLoginStatus() {return loginStatus;}
+
+	/**
+	 * loginStatus setter
+	 * @param loginStatus the new loginStatus for this User
+	 */
+	public void setLoginStatus(boolean loginStatus) {this.loginStatus = loginStatus;}
 
 }
