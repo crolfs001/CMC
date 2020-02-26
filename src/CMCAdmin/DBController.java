7312320b84963a3ab28b0c23a33ab34079c18901
DMURPHY001 <dmurphy001@vplincsci011.ad.csbsju.edu>
2020-02-26 06:56:40 -0600

    create constructor for DBController

--- src/CMCAdmin/DBController.java
+++ src/CMCAdmin/DBController.java
@@ -1,9 +1,23 @@
+/**
+ * DBController class holds the systems information, holding all the different
+ * user account information as well as the information on the different
+ * Universities
+ */
 package CMCAdmin;
+import java.util.ArrayList;
+import CMCUser.User;
+import  CMCUniversity.University;
 
 public class DBController {
+	private ArrayList<User> userList;
+	private ArrayList<University> schoolList;
 
+	/**
+	 * constructs a  DBController object
+	 */
 	public DBController() {
-		// TODO Auto-generated constructor stub
+		userList = new ArrayList<User>();
+		schoolList = new ArrayList<University>();
 	}
 
 }
