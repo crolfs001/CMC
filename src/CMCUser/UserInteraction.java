f4ae56104c804324b6d386cceb422e4bb8dd06c4
DMURPHY001 <dmurphy001@vplincsci025.ad.csbsju.edu>
2020-02-25 18:32:17 -0600

    create UserInteraction constructor

--- src/CMCUser/UserInteraction.java
+++ src/CMCUser/UserInteraction.java
@@ -1,19 +1,23 @@
 /**
+	This class is used to create and manipulate the UserInteraction object.
+	This is the class that an actual, living user interacts with when they interact with the system.
  * 
  */
 package CMCUser;
 
 /**
- * @author dmurphy001
+ * @author Devin Murphy
  *
  */
 public class UserInteraction {
+	private User user;
 
 	/**
-	 * 
+	 * Constructs a UserInteraction object
+	 * @param user the User object for this UserInteraction
 	 */
-	public UserInteraction() {
-		// TODO Auto-generated constructor stub
+	public UserInteraction(User user) {
+		this.user = user;
 	}
 
 }
