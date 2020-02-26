1879fbc5c7455779bd720426926f327fdb187294
DMURPHY001 <dmurphy001@vplincsci025.ad.csbsju.edu>
2020-02-25 18:01:28 -0600

    created Constructor for user class

--- src/CMCUser/User.java
+++ src/CMCUser/User.java
@@ -4,16 +4,34 @@
 package CMCUser;
 
 /**
- * @author dmurphy001
+ * @author Devin Murphy
  *
  */
 public class User {
+	private String firstName, lastName;
+	private String userName, password;
+	private char type, status;
+	private boolean loginStatus;
 
 	/**
+	 * Constructs a User object
 	 * 
+	 * @param firstName the first name of the user
+	 * @param lastName the last name of the user
+	 * @param userName the userName for the user's account
+	 * @param password the password for the user's account
+	 * @param type the type of account, 'a' for admin or 'u' for non-admin
+	 * @param status the status of the account, 'Y' for activated and 'N' for deactivated
 	 */
-	public User() {
-		// TODO Auto-generated constructor stub
+	public User(String firstName, String lastName, String userName, String password, 
+			char type) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.type = type;
+		this.status = 'Y'; //default is activated
+		this.loginStatus = false; //default is user is not logged in
 	}
 
 }
