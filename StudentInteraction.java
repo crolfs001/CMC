3a7f1e6fb1d691651176e5c077f9b67406c972cc
lyang001 <61028107+lyang001@users.noreply.github.com>
2020-03-10 15:45:33 -0500

    Add files via upload

---
+++ StudentInteraction.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package interaction;
+
+import java.util.List;
+
+import Student.Student;
+
+/**
+ * @author lyang001
+ *
+ */
+public class StudentInteraction {
+	private Student student;
+	/**
+	 * 
+	 */
+	public StudentInteraction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void openViewSavedSchools(){
+		
+	}
+	
+	public university pressSaveButton (University school) {
+		
+		return school;
+	}
+	public void viewSchoolInfo() {
+		
+	}
+	public university displayTopFive(List<String> recSchools) {
+		
+		return recSchools;
+		
+	}
+	public void viewProfilPage() {
+		
+	}
+	public void promptRemove() {
+		
+	}
+	public List<String> searchSchool(){
+		List<university> searchResult;
+		return searchResult;
+	}
+	public void resetSearch() {
+		
+	}
+	public void confirmRemove() {
+		
+	}
+	public Student cancelChange(Student s) {
+		return s;	
+	}
+	public void makeEditAble() {
+		
+	}
+}
