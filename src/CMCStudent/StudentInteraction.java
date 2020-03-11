2f1f3010d330207a9d79777ca2bdf9f7bcd079a3
DMURPHY001 <dmurphy001@vplincsci001.ad.csbsju.edu>
2020-03-10 20:09:12 -0500

    resolved merge conflicts

--- src/CMCStudent/StudentInteraction.java
+++ src/CMCStudent/StudentInteraction.java
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 package CMCStudent;
 
 import java.util.ArrayList;
 import CMCStudent.Student;
 import CMCUniversity.University;
-=======
+import CMCUniversity.UniversityController;
+
 import java.util.List;
 import CMCUniversity.University;
 
->>>>>>> 227a0fc70f672924f06ff15fe34b7207b398d9a0
 /**
  * @author lyang001
  * The interactions that a student will go thorugh when navigating the program
@@ -17,23 +16,20 @@ public class StudentInteraction {
 	// An instance variable for a student to be used in the interaction
 	private Student student;
 	private String uniName;
+	private UniversityController universityController;
 	
 	/**
 	 * 
 	 */
 	public StudentInteraction(String uniName) {
 		this.uniName = uniName;
+		this.universityController = null;
 	}
 
 
 	public void openViewSavedSchools(){
 		
 	}
-<<<<<<< HEAD
-	
-	public String pressSaveButton() {
-		return this.uniName;
-=======
 
 	/**
 	*@University requires a university to save
@@ -42,40 +38,30 @@ public class StudentInteraction {
 	public University pressSaveButton (University school) {
 		
 		return school;
->>>>>>> 227a0fc70f672924f06ff15fe34b7207b398d9a0
 	}
-	public void viewSchoolInfo() {
-		
-	}
-<<<<<<< HEAD
-	/*public University displayTopFive(ArrayList<String> recSchools) {
-=======
+
+
 	/**
+	* finds the five closest schools for an input school name
 	* @List requires a list of recommeneded schools to return
 	* @return returns a list of the top 5 recommended schools for a university
 	*/
-	public university displayTopFive(List<String> recSchools) {
->>>>>>> 227a0fc70f672924f06ff15fe34b7207b398d9a0
+	public ArrayList<String> displayTopFive(String universityName) {		
+		return universityController.getTopFiveClosestSchool(universityName);
 		
-		return recSchools;
-		
-	}*/
+	}
 	public void viewProfilPage() {
 		
 	}
 	public void promptRemove() {
 		
 	}
-<<<<<<< HEAD
-	public ArrayList<University> searchSchool(){
-		ArrayList<University> searchResult = new ArrayList<University>();
-=======
+
 	/**
 	* @return Returns a list of schools that match the criteria the student searched for
 	*/
 	public List<String> searchSchool(){
-		List<University> searchResult;
->>>>>>> 227a0fc70f672924f06ff15fe34b7207b398d9a0
+		List<String> searchResult;
 		return searchResult;
 	}
 	public void resetSearch() {
@@ -92,6 +78,8 @@ public class StudentInteraction {
 		return s;	
 	}
 	public void makeEditAble() {
+	}
+}
 		
 
-}
+
