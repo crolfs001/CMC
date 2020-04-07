21ea0d7add976f2b0e32e734871ba187b3e04702
DMURPHY001 <dmurphy001@vplincsci009.ad.csbsju.edu>
2020-04-07 16:16:53 -0500

    created LogoutTest to test logout functionality

--- test/AllTests.java
+++ test/AllTests.java
@@ -18,7 +18,7 @@ import FunctionalityTests.*;
 @RunWith(Suite.class)
 @SuiteClasses({DBControllerTest.class,DriverTest.class,StudentFunctionalityControllerTest.class,StudentInteractionTest.class,
 	StudentTest.class,UniversityControllerTest.class,UniversityTest.class, UserSchoolTest.class,AccountControllerTest.class,
-	UserInteractionTest.class,UserTest.class,LoginTest.class})
+	UserInteractionTest.class,UserTest.class,LoginTest.class,LogoutTest.class})
 public class AllTests {
 
 }
