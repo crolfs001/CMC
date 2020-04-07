15a00651bbaec9a2d033562ba161f356ac2ba5ab
htwang021 <hwang003@csbsju.edu>
2020-04-06 19:09:55 -0500

    add addSchoolTest() method to UserSchoolTest

--- test/CMCUniversity/UserSchoolTest.java
+++ test/CMCUniversity/UserSchoolTest.java
@@ -9,17 +9,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import CMCUser.User;
+
 /**
  * @author crolfs001
  *
  */
 public class UserSchoolTest {
-
+	private UserSchool testUserSchool;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		User user1 = new User("Hongtao", "Wang", "hwang003", "fakePassword", 'u'); 
+		this.testUserSchool = new UserSchool(user1);
 	}
 
 	/**
@@ -30,8 +34,10 @@ public class UserSchoolTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testaddSchool() {
+		University uni = new University("CSBSJU", "MN", "Colegeville", "Private", 3000, 0.50, 1.00, 1.00,1.00, 1.00, 20, 0.5, 0.5, 10, 10, 10);
+		this.testUserSchool.addSchool("hwang003", uni);
+		Assert.assertEquals("The name of university should be 'CSBSJU'", "CSBSJU", this.testUserSchool.savedUniversityList.get(0).getName());
 	}
 
 }
